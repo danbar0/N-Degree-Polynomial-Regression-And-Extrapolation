9ffcd38c47bde855d32d2832d8a9882635da7a3a
danbar0 <35388915+danbar0@users.noreply.github.com>
2020-03-22 13:57:46 -0500

    Started adding kivy stuff

---
+++ extrapolation.py
@@ -0,0 +1,49 @@
+import openpyxl
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+dates = []
+totals = []
+
+extrapolation = 1000
+
+def plot_values(filepath):
+    str(filepath)
+    try:
+        wb = openpyxl.load_workbook(filepath)
+        print(filepath)
+    except openpyxl.utils.exceptions.InvalidFileException:
+        print("Invalid file name!")
+
+        return
+
+    sheet = wb.get_sheet_by_name('Sheet1')
+
+    for i in range(2, sheet.max_row):
+        temp = sheet.cell(row=i, column=1).value
+        print(temp)
+        dates.append(temp.date())
+        totals.append(sheet.cell(row=i, column=2).value)
+
+    print(totals)
+    x = mdates.date2num(dates)
+    y = totals
+
+    poly_degree = 2
+    z4 = np.polyfit(x, totals, poly_degree)
+    p4 = np.poly1d(z4)
+
+    print(p4)
+
+    fig, cx = plt.subplots()
+
+    xx = np.linspace(x.min(), x.max()+extrapolation, 1000)
+    dd = mdates.num2date(xx)
+
+    cx.plot(dd, p4(xx), '-g')
+    cx.plot(dates, y, '+', color='b', label='blub')
+
+    cx.grid()
+    cx.set_ylim(0, 100000)
+    plt.show()
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import openpyxl
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
+from extrapolation import plot_values
 
 try:
     import kivy
@@ -9,90 +6,56 @@ except ImportError:
     raise ImportError("You don't have Kivy installed!")
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.filechooser import FileChooserIconView
-from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.lang import Builder
 
-filename = "Savings.xlsx"
-dates = []
-totals = []
+Builder.load_string('''
+#:kivy 1.10.0
 
-extrapolation = 2000
+<SaveFile>:
+    title: 'Save File'
 
+    # FileChooserLayout
+    BoxLayout:
+        orientation: 'vertical'
 
-def plotValues(*args):
-    try:
-        wb = openpyxl.load_workbook(filename)
-        print(filename)
-    except openpyxl.utils.exceptions.InvalidFileException:
-        print("Invalid file name!")
+        # ButtonArea
+        BoxLayout:
+            orientation: 'horizontal'
+            spacing: 50
+            size_hint: (.5,.5)
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
 
-        return
+            FileChooserListView:
+                on_selection: 
+                    app.root.selected_file(*args)
 
-    sheet = wb.get_sheet_by_name('Sheet1')
+<RootWidget>:
 
-    for i in range(3, sheet.max_row-1):
-        temp = sheet.cell(row=i, column=1).value
-        dates.append(temp.date())
-        totals.append(sheet.cell(row=i, column=2).value)
-
-    x = mdates.date2num(dates)
-    y = totals
-
-    poly_degree = 3
-    z4 = np.polyfit(x, totals, poly_degree)
-    p4 = np.poly1d(z4)
-
-    print(p4)
-
-    fig, cx = plt.subplots()
-
-    xx = np.linspace(x.min(), x.max()+extrapolation, 1000)
-    dd = mdates.num2date(xx)
-
-    cx.plot(dd, p4(xx), '-g')
-    cx.plot(dates, y, '+', color='b', label='blub')
-
-    cx.grid()
-    cx.set_ylim(0, 1000000)
-    plt.show()
+''')
 
 
-def example_function(*args):
-    global filename
-    print(args)
-    filename = str(args)
+class SaveFile(Popup):
+    pass
 
 
-def button_func(*args):
-    print("pressed")
+class RootWidget(BoxLayout):
 
-
-class HomeScreen(GridLayout):
     def __init__(self, **kwargs):
-        super(HomeScreen, self).__init__(**kwargs)
-        self.cols = 3
-        self.rows = 3
+        super(RootWidget, self).__init__(**kwargs)
+        filepop = SaveFile()
+        filepop.open()
 
-        self.file_chooser = FileChooserIconView(path = '/home/')
-
-        self.go_button = Button(text="Extrapolate!", on_press=plotValues)
-        self.add_widget(self.go_button)
-
-        self.add_widget(Label(text='File location'))
-        self.username = TextInput(multiline=False)
-        #self.username.bind(text=example_function)
-        self.add_widget(self.username)
+    def selected_file(self, *args):
+        print("args=", args[1])
+        plot_values(args[1])
 
 
-class MyApp(App):
+class DemoApp(App):
     def build(self):
-        return HomeScreen()
+        return RootWidget()
 
 
-if __name__ == "__main__":
-    #plotValues()
-    MyApp().run()
+if __name__ == '__main__':
+    DemoApp().run()
